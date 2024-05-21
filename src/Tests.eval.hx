1937d599f2dcda83397c0794c095cefc55c09165
Joey <9813078+PXshadow@users.noreply.github.com>
2024-05-21 07:31:20 +0200

    Tests: nodejs source map support, more loggin, add more std libs

--- src/Tests.eval.hx
+++ src/Tests.eval.hx
@@ -3,6 +3,11 @@ import sys.io.File;
 
 final libs = [
     "unicode",
+    "unicode/utf8",
+    "unicode/utf16",
+    "math",
+    "strings",
+    "bytes",
 ];
 
 var runnningCount = 0;
@@ -34,22 +39,22 @@ function wait() {
 
 function createHxmls() {
     for (i in 0...libs.length) {
-        runCommand('haxelib run go2hx -compiler_interp -nodep ${libs[i]} --norun --test --hxml $i.hxml');
+        runCommand(libs[i], 'haxelib run go2hx -compiler_interp -nodep ${libs[i]} --norun --test --hxml $i.hxml');
     }
 }
 
 function createHxb() {
-    runCommand('haxelib run go2hx hxb golibs');
+    runCommand('hxb', 'haxelib run go2hx hxb golibs');
 }
 
 final targets = [
-    "hl", "js", //"jvm",//, "cpp"
+    "hl", //"js", //"jvm",//, "cpp"
 ];
 
 function buildHxmls() {
     for (i in 0...libs.length) {
         for (target in targets) {
-            runCommand('haxe $i.hxml --hxb-lib go2hxlibs.zip ' + buildTarget(target, '$i'));
+            runCommand(libs[i], 'haxe $i.hxml --hxb-lib go2hxlibs.zip ' + buildTarget(target, '$i'));
         }
     }
 }
@@ -58,25 +63,24 @@ function runHxmls() {
     for (i in 0...libs.length) {
         for (target in targets) {
             if (target != "interp") {
-                runCommand(runTarget(target, '$i', [], mainFromHxml('$i.hxml')));
+                runCommand(libs[i], runTarget(target, '$i', [], mainFromHxml('$i.hxml')));
             }
         }
     }
 }
 
-function runCommand(command:String) {
-    Sys.println(command);
+function runCommand(libName:String, command:String) {
+    Sys.println('$libName : $command');
     runnningCount++;
     var code = 0;
     tp.run(() -> {
         code = Sys.command(command);
         if (code != 0) {
-            Sys.println("failed to run command: " + command);
+            Sys.println('$libName failed to run command: $command');
             tp.shutdown();
-            cleanup();
             Sys.exit(1);
         }
-        trace("FINISH COMMAND: " + code + " " + command);
+        trace('FINISH COMMAND: $code $command');
         runningCountMutex.acquire();
         runnningCount--;
         runningCountMutex.release();
@@ -107,8 +111,10 @@ function buildTarget(target:String, out:String):String {
 	return switch target {
         case "jvm":
             '--$target build/$out.jar';
-        case "hl", "js":
+        case "hl":
             '--$target build/$out.$target';
+        case "js":
+            '-lib hxnodejs --$target build/$out.$target';
 		case "cpp":
 			'--$target build/$target/$out';
 		case "interp":
@@ -138,7 +144,7 @@ function runTarget(target:String, out:String, args:Array<String>, main:String):S
 		case "hl":
 			'$target build/$out.hl';
 		case "js":
-			'node build/$out.js';
+			'NODE_OPTIONS=--enable-source-maps node build/$out.js';
 		default:
 			throw "unknown target: " + target;
 	};
